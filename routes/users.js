cbf89ba12a945105fff7b94b9db38facc5c0976d
ardisrtilson <email@example.com>
2021-08-17 16:28:50 -0500

    Fixed secret GET

--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,7 @@ async function getToken() {
   let data = {
     'grant_type': "client_credentials",
     'client_id': "admin-cli",
-    'client_secret': "0f6b574b-821e-47fe-ad58-aa8d6fcf9fbe"
+    'client_secret': "16e6581f-6bad-44b5-b0dc-012d2c77bbc5"
   }
 
   let url = 'https://keycloak-service-dot-tj-node-server-322619.ue.r.appspot.com/auth/realms/demo-realm/protocol/openid-connect/token'
@@ -36,6 +36,27 @@ async function getToken() {
   
   return res
 }
+
+// async function getToken() {
+
+//   let config = {
+//     headers: {"Content-Type": "application/x-www-form-urlencoded"}
+//     }
+  
+//   let data = {
+//     'grant_type': "password",
+//     'client_id': "admin-cli",
+//     'username': "admin",
+//     'password': "44qxQPtkcA"
+//   }
+
+//   let url = 'https://keycloak-service-dot-tj-node-server-322619.ue.r.appspot.com/auth/realms/master/protocol/openid-connect/token'
+
+//   let res = await axios.post(url, qs.stringify(data), config
+//   ).catch(error => console.log(error))
+  
+//   return res
+// }
 
 async function createUser(token, stats) {
 
