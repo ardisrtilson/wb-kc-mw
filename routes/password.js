29a4a367bc06357ccc8e8c5d8e54a29cfed9a38c
ardisrtilson <email@example.com>
2021-08-19 13:29:59 -0500

    installed cors

---
+++ routes/password.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+
+router.put('/', function requestHandler(req, res) {
+    res.send(req.body)
+    getToken().then(response => {
+        const decrypted = jwt_decode(response.data.access_token)
+        changePassword(response, decrypted.id, req.body)})
+    }
+  );
+
+  router.get('/', function(req, res, next) {
+    getToken().then(response => {
+
+        res.send(decrypted)
+    })
+});
+
+  async function getToken() {
+
+  let config = {
+    headers: {"Content-Type": "application/x-www-form-urlencoded"}
+    }
+  
+  let data = {
+    'grant_type': "password",
+    'client_id': "admin-cli",
+    'username': "rabbit",
+    'password': "minty"
+  }
+
+  let url = 'https://keycloak-service-dot-tj-node-server-322619.ue.r.appspot.com/auth/realms/master/protocol/openid-connect/token'
+
+  let res = await axios.post(url, qs.stringify(data), config
+  ).catch(error => console.log(error))
+  
+  return res
+}
+
+async function changePassword(token, stats) {
+
+    let config = {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token.data.access_token}`
+      }
+    }
+    
+    let data = {"type":`${stats.type}`,"temporary":`${stats.temporary}`, "value":`${stats.value}`}
+  
+    let url = `/auth/admin/realms/demo-realm/users/{id}/reset-password`
+  
+    await axios.post(url, JSON.stringify(data), config).catch(error => console.log(error))
+    
+  }
+
+module.exports = router;
