d9c22add92b23a992d14cd73835394f5c55ce5fc
ardisrtilson <email@example.com>
2021-08-19 15:06:23 -0500

    Realm permissions fix

--- routes/users.js
+++ routes/users.js
@@ -43,14 +43,13 @@ async function getToken() {
 //     headers: {"Content-Type": "application/x-www-form-urlencoded"}
 //     }
   
-//   let data = {
-//     'grant_type': "password",
-//     'client_id': "admin-cli",
-//     'username': "admin",
-//     'password': "44qxQPtkcA"
-//   }
+//     let data = {
+//       'grant_type': "client_credentials",
+//       'client_id': "admin-cli",
+//       'client_secret': "16e6581f-6bad-44b5-b0dc-012d2c77bbc5"
+//     }
 
-//   let url = 'https://keycloak-service-dot-tj-node-server-322619.ue.r.appspot.com/auth/realms/master/protocol/openid-connect/token'
+//   let url = 'https://keycloak-service-dot-tj-node-server-322619.ue.r.appspot.com/auth/realms/demo-realm/protocol/openid-connect/token'
 
 //   let res = await axios.post(url, qs.stringify(data), config
 //   ).catch(error => console.log(error))
