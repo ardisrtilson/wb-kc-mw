e5065b015bcb1795fc491d9d4d11d63e95c785e3
ardisrtilson <email@example.com>
2021-08-19 13:34:54 -0500

    Installed cors

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var decryptRouter = require('./routes/decrypt');
 var passwordRouter = require('./routes/password');
-
+var cors = require('cors')
 var app = express();
 
 // view engine setup
+app.use(cors())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
